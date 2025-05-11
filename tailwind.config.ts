868c70e45c83b7961889ac376b3091fa181d5b57
KidusBasazinew <kidusbw@gmail.com>
2025-05-11 06:46:40 -0700

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -27,6 +27,7 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.css",
   ],
   theme: {
     extend: {
